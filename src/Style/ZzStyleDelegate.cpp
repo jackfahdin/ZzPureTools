45e6c3b8f6359ccf803cfd8c9e0517a9c89f9404
Jackfahdin.Guo <guomaojie@pudutech.com>
2026-06-25 09:27:10 +0000

    添加缺失的头文件

--- src/Style/ZzStyleDelegate.cpp
+++ src/Style/ZzStyleDelegate.cpp
@@ -1,5 +1,7 @@
 #include "ZzFluent/Style/ZzStyleDelegate.hpp"
 
+#include "ZzFluent/Core/ZzTheme.hpp"
+
 QSize ZzStyleDelegate::sizeHint(const ZzStyleContext& context, const QSize& available) const
 {
     const int height = context.theme != nullptr ? context.theme->metrics().controlHeight : 32;
